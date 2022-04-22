5db7f24c562f0ed71cba169586c94c53035fb6c4
JonathanLima25 <jonathan.ferreira6@fatec.sp.gov.br>
2022-04-22 11:13:44 -0300

    config server.js

---
+++ server.js
@@ -0,0 +1,7 @@
+import express from "express";
+
+const app = express();
+const port = process.env.PORT || 3000
+
+app.use(express.static('public'));
+app.listen(port, () => console.log(`App disponível em http://localhost/${port}`))
