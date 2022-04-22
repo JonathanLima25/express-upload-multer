0e759b38954b7b7398d7dfe2aefc2c6314251a21
JonathanLima25 <jonathan.ferreira6@fatec.sp.gov.br>
2022-04-22 11:43:24 -0300

    upload with multer

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 import express from "express";
 import multer from "multer";
+import path from "path";
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
         cb(null, 'uploads/');
     },
     filename: function (req, file, cb) {
-        cb(null, file.fieldname + '-' + Date.now())
+        cb(null, `${file.fieldname}-${Date.now()}.${path.extname(file.originalname)}`)
     }
 });
 
