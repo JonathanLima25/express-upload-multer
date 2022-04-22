ec0b2330669b5b7f4b09753c53db7315d0312cec
JonathanLima25 <jonathan.ferreira6@fatec.sp.gov.br>
2022-04-22 11:24:07 -0300

    add storage

--- server.js
+++ server.js
@@ -1,7 +1,23 @@
 import express from "express";
+import multer from "multer";
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'uploads/');
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.fieldname + '-' + Date.now())
+    }
+});
+
+const upload = multer({ storage });
 
 const app = express();
 const port = process.env.PORT || 3000
 
 app.use(express.static('public'));
+
+app.post('/file/upload', upload.single('file'),
+    (req, res) => res.send('<h2>Upload realizado com sucesso!</h2>'));
+
 app.listen(port, () => console.log(`App disponível em http://localhost/${port}`))
